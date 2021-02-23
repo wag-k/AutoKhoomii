5366a85abec9d3d51093d1cfb28ba95e0a4494df
wag-k <captenkenta@gmail.com>
2021-02-23 16:12:21 +0900

    FirstCommit feat: ボタンを押すと音が鳴る機能

---
+++ KhoomiiPlayer.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoKhoomii
+{
+    class KhoomiiPlayer
+    {
+        public void Run(){
+            MemoryStream waveStream = CreateWave();
+            PlayKhoomii(ref waveStream);
+            waveStream.Close();
+        }
+        
+        public MemoryStream CreateWave(){
+            const uint sampleRate = 44100;  // サンプリング周波数
+
+            // 波形データの生成
+            uint wavelen = (uint)(sampleRate * 1.0);
+            byte[] wave = new byte[wavelen];
+
+            double t = 0;
+            for (uint i = 0; i < wavelen; i++)
+            {
+                t = (t + 880.0 / sampleRate) % 1;
+                wave[i] = (byte)(128 + Math.Sin(2 * Math.PI * t) * (1 - i / (double)wavelen) * 10);
+            }
+
+            //using (FileStream st = new FileStream(@"c:\tmp\hoge.wav", FileMode.Create))
+            MemoryStream st = new MemoryStream();
+        
+            // WAVEファイルヘッダ
+            WriteStr(st, "RIFF");
+            WriteVal(st, 4, wavelen + 36);
+            WriteStr(st, "WAVE");
+            WriteStr(st, "fmt ");
+            WriteVal(st, 4, 16);
+            WriteVal(st, 2, 0x0001);         // WAVE_FORMAT_PCM
+            WriteVal(st, 2, 1);              // nChannels
+            WriteVal(st, 4, sampleRate);     // nSamplesPerSec
+            WriteVal(st, 4, sampleRate);     // nAvgBytesPerSec
+            WriteVal(st, 2, 1);              // nBlockAlign
+            WriteVal(st, 2, 8);              // wBitsPerSample
+            WriteStr(st, "data");
+            WriteVal(st, 4, wavelen);
+
+            st.Write(wave, 0, wave.Length);
+
+            st.Seek(0, SeekOrigin.Begin); // Seekを原点にしておく。
+
+            return st;
+        }
+
+        private void WriteVal(Stream st, int len, uint value)
+        {
+            byte[] ba = BitConverter.GetBytes(value);
+            st.Write(ba, 0, len);
+        }
+
+        private void WriteStr(Stream st, string str)
+        {
+            byte[] ba = Encoding.ASCII.GetBytes(str);
+            st.Write(ba, 0, ba.Length);
+        }
+
+        public void PlayKhoomii(ref MemoryStream wave){
+            wave.Seek(0, SeekOrigin.Begin);
+            var player = new SoundPlayer(wave);
+            player.PlaySync();
+        }
+    }
+}
---
+++ WaveFIleHeader.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace AutoKhoomii
+{
+    /// <summary>
+    /// 以下の記事そのまま
+    /// [C#でWAVE再生] https://qiita.com/Stosstruppe/items/4d5e1511d082471d1cad
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential)]
+    class WaveFileHeader
+    {
+        public uint riff_ckid = 0x46464952;    // "RIFF"
+        public uint riff_cksize;
+        public uint fccType = 0x45564157;      // "WAVE"
+        public uint fmt_ckid = 0x20746d66;     // "fmt "
+        public uint fmt_cksize = 16;
+        public ushort wFormatTag = 0x0001;     // WAVE_FORMAT_PCM
+        public ushort nChannels;
+        public uint nSamplesPerSec;
+        public uint nAvgBytesPerSec;
+        public ushort nBlockAlign;
+        public ushort wBitsPerSample;
+        public uint data_ckid = 0x61746164;    // "data"
+        public uint data_cksize;
+    }
+}
